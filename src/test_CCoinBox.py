abb7c31152cf9d252d3b31d310ff84627a924e33
Pastroy2002 <gabrielcloutier2002@gmail.com>
2024-10-21 11:09:42 -0400

    mutant killing test

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -6,6 +6,11 @@ class Test_CCoinBox(unittest.TestCase):
     def test_pass(self):
         pass
 
+    def test_ajouter_un_25c(self):
+        coinBox = CCoinBox()
+        coinBox.ajouter_25c()
+        self.assertEqual(coinBox.get_vente_permise(), False)
+
     def test_monnaie(self):
         coinBox = CCoinBox()
         coinBox.ajouter_25c()
